8d6d3919885cf88571e61f8c22385ea16abf0598
Eliseu Damasceno <eliseudamasceno@gmail.com>
2021-02-05 13:18:42 -0300

    Uploading GoBarber back-end

---
+++ back-end/src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,42 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+   user_id: string;
+   avatarFilename: string;
+}
+@injectable()
+class UpdateUserAvatarService {
+   constructor(
+      @inject('StorageProvider')
+      private storageProvider: IStorageProvider,
+
+      @inject('UsersRepository')
+      private usersRepository: IUsersRepository,
+   ) {}
+
+   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+      const user = await this.usersRepository.findById(user_id);
+      if (!user) {
+         throw new AppError('Only authenticated users can change avatar.', 401);
+      }
+
+      if (user.avatar) {
+         await this.storageProvider.deleteFile(user.avatar);
+      }
+
+      const filename = await this.storageProvider.saveFile(avatarFilename);
+
+      // Atualizar novo avatar
+      user.avatar = filename;
+      await this.usersRepository.save(user);
+
+      return user;
+   }
+}
+
+export default UpdateUserAvatarService;
